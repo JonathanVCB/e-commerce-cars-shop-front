7f139ff15cdadaf616f31d3f1a9ba92312351874
leo@DESKTOP-H3DCF6T <leonardoneveswork@gmail.com>
2023-04-07 22:02:18 -0300

    add: created component car card

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./styles/global.style";
 import { BrowserRouter } from "react-router-dom";
+import CarCard from "./components/cards/car/car";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <GlobalStyle />
-      <App />
+      {/* <App /> */}
+      <CarCard />
     </BrowserRouter>
   </React.StrictMode>
 );
